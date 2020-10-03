08650bbaab7320cea07186ac15a637e5b3e45e35
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-10-02 22:49:54 -0300

    Adding content to class user, total_amount was created but empty

--- bankaccount.rb
+++ bankaccount.rb
@@ -1,9 +1,19 @@
 class User
 
+    attr_reader :user_name,
+    def initialize( user_name, account = [])
+        @user_name = user_name
+        @account = account
+    end
+
+    def total_amount
+        
+    end
     
 end
 
 class BankAccount
+    
     attr_accessor :bank_name,
     :number_account, :balance
 
@@ -13,9 +23,15 @@ class BankAccount
         @balance = balance
     end
 
-    def to_transfer()
-    
+    def to_transfer( amount, another_account )
+
+        @balance = balance - amount
+
+        another_account.balance = another_account.balance + amount
+
     end
 
 end
+
+
 
