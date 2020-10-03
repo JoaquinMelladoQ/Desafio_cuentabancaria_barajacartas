916bf85b204bb2a95c2d7271ff334d004ec23cf5
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-10-02 23:01:08 -0300

    Total_balance method was added with logic

--- bankaccount.rb
+++ bankaccount.rb
@@ -1,37 +1,38 @@
-class User
-
-    attr_reader :user_name,
-    def initialize( user_name, account = [])
-        @user_name = user_name
-        @account = account
-    end
-
-    def total_amount
-        
-    end
-    
-end
-
 class BankAccount
-    
+
     attr_accessor :bank_name,
     :number_account, :balance
-
+    
     def initialize( bank_name, number_account, balance = 0 )
         @bank_name = bank_name
         @number_account = number_account
         @balance = balance
     end
-
+    
     def to_transfer( amount, another_account )
-
-        @balance = balance - amount
-
-        another_account.balance = another_account.balance + amount
-
+        @balance = self.balance - amount
+        another_account.self.balance = another_account.self.balance + amount
     end
 
 end
 
+class User
+    
+    attr_reader :user_name
+
+    def initialize( user_name, account = [])
+        @user_name = user_name
+        @account = account
+    end
+
+    def total_balance
+        sum_total = 0
+        self.account.each do | iter |
+            sum_total += iter.balance
+        end
+        return sum_total
+    end
+
+end
 
 
