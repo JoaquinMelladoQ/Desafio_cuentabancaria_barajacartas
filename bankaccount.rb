cb8097ac2fe0d7519f9bef24088428904434f37b
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-10-02 22:28:01 -0300

    Attr_accessor was added to method and instance variables

--- bankaccount.rb
+++ bankaccount.rb
@@ -4,9 +4,13 @@ class User
 end
 
 class BankAccount
-    
+    attr_accessor :bank_name,
+    :number_account, :balance
+
     def initialize( bank_name, number_account, balance = 0 )
-        
+        @bank_name = bank_name
+        @number_account = number_account
+        @balance = balance
     end
 
     def to_transfer()
